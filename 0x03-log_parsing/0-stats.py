3f10afe1ac7c09122dff4b85947beebe969fd0d9
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-17 00:33:18 +0100

    extract information from a log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,33 +22,35 @@ script that reads stdin line by line and computes metrics:
 import sys
 
 
+import sys
+import re
+
+
 def log_parsing():
     """gets infomation from logs"""
     codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     status_count = {status: 0 for status in codes}
-    file_size = 0
+    file_size = []
     count = 0
     status = None
     try:
         for line in sys.stdin:
-            line = line.split()
-            if len(line) > 4:
-                status = line[-2]
-                size = line[-1]
+            count += 1
+            search = re.search('GET .* (.*) (.*)', line)
+            status = search.group(1)
             if status in status_count.keys():
                 status_count[status] += 1
-            file_size += int(size)
-            count += 1
+                file_size.append(int(search.group(2)))
             if count == 10:
                 count = 0
-                print("File size: {}".format(file_size))
+                print("File_size: {}".format(sum(file_size)))
                 for key, val in sorted(status_count.items()):
                     if val != 0:
                         print("{}: {}".format(key, val))
     except KeyboardInterrupt:
         pass
     finally:
-        print("File size: {}".format(file_size))
+        print("File size: {}".format(sum(file_size)))
         for key, val in sorted(status_count.items()):
             if val != 0:
                 print("{}: {}".format(key, val))

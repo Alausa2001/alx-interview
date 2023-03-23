8cc126900fd3bea53bc2929d5e3b50736dc35b28
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-23 13:38:24 +0100

    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """
+    Get the no if minimum combination
+    """
+    if total <= 0:
+        return 0
+    amt = [float('inf')] * (total + 1)
+    amt[0] = 0
+
+    for coin in coins:
+        for fewest in range(coin, total + 1):
+            amt[fewest] = min(amt[fewest], amt[fewest - coin] + 1)
+            # amt[fewest] += 1
+    if amt[total] != float('inf'):
+        return amt[total]
+    return - 1
